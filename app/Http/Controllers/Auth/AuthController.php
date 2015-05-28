fbf64e7583fd809478ee5149e2fcf67325e1341d
albafo <alvaro.bano.fos@gmail.com>
2015-05-29 00:43:28 +0200

    asdsa

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -186,8 +186,9 @@ class AuthController extends Controller {
     }
 
     public function getTest() {
-        echo gethostbyname('www.google.es');
-        file_get_contents('http://www.google.es');
+        $opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
+        $context = stream_context_create($opts);
+        $header = file_get_contents('http://www.google.com',false,$context);
     }
 
 }
